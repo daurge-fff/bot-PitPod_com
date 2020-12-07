3ac5cbc36eff818b873f468e15a7b950e127b630
Garold <daurge_fff@mail.ru>
2020-12-07 20:29:46 +0500

    Create pit_aio.py
    Учимся читать документацию!

---
+++ pit_aio.py
@@ -0,0 +1,40 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+API_TOKEN = 'BOT TOKEN HERE'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+class UserForm(StatesGroup):
+    name = State()  # Will be represented in storage as 'Form:name'
+    age = State()  # Will be represented in storage as 'Form:age'
+    gender = State()  # Will be represented in storage as 'Form:gender'
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+    
+URL = 'https://docs.aiogram.dev/en/dev-2.x/_static/logo.png'
+
+@dp.message_handler(commands=['image, img'])
+async def cmd_image(message: types.Message):
+    await bot.send_photo(message.chat.id, types.InputFile.from_url(URL))
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
